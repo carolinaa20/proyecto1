5ba8dc399325b03b3491eb6eecf31ef987f7dd7e
Dianadiaz <dianacarolinaa2024@gmail.com>
2024-04-08 11:13:08 -0500

    cambios pagina total

---
+++ js/boton.js
@@ -0,0 +1,9 @@
+const respuestaUsuario = confirm("Quieres usar tema oscuro?");
+
+if(respuestaUsuario ===true){
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "black";
+} else {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
