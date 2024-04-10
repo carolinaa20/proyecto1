c09f915dd8baa567b8ae37b1f48da1919ec8990b
Dianadiaz <dianacarolinaa2024@gmail.com>
2024-04-10 09:53:39 -0500

    cambios

--- js/boton.js
+++ js/boton.js
@@ -1,9 +1,19 @@
-const respuestaUsuario = confirm("Quieres usar tema oscuro?");
+let isBlack = false;  
 
-if(respuestaUsuario ===true){
-  document.body.style.backgroundColor = "black";
-  document.body.style.color = "black";
-} else {
-  document.body.style.backgroundColor = "black";
-  document.body.style.color = "white";
-}
+const btn = document.querySelector("#darkModeButton");
+const gridItem = document.querySelectorAll(".grid-item");
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if(isBlack){
+    document.body.style.backgroundColor = "#f3e5d8";
+    document.body.style.color = "black";
+    isBlack = false
+
+  }else{
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "black";
+    gridItem.forEach(item => item.style.backgroundColor = "#2F2F2F");
+    gridItem.forEach(item => item.style.color = "white");
+    isBlack = true
+  }
+});
